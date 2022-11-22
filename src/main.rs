9f2f2b1feba4551e3a291676eb926f78ab53c217
Gerald Scharitzer <no@e.mail>
2022-11-23 00:01:35 +0100

    input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
     println!("Advent of Code");
 	day1();
@@ -5,4 +8,9 @@ fn main() {
 
 fn day1() {
 	println!("Day 1: Not Quite Lisp");
+	let file = File::open("day1.in").expect("open input failed");
+	let mut reader = BufReader::new(file);
+	let mut line = String::new();
+	reader.read_line(&mut line).expect("read line failed");
+	println!("{line}");
 }
