da2e0f72f0f3ba2b3d936cabc888f176250314b1
gerald-scharitzer <gerald.scharitzer@gmail.com>
2022-11-25 00:05:45 +0100

    lib day1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 pub struct Puzzle<'a> {
     year: i32,
     day: i32,
@@ -12,4 +15,56 @@ impl<'a> Puzzle<'a> {
     pub fn get_year(&self) -> i32 { self.year }
     pub fn get_day(&self) -> i32 { self.day }
     pub fn get_name(&self) -> &str { self.name }
+}
+
+pub fn day1part1() -> Result<(), String> {
+	let puzzle = Puzzle::new(2015, 1, "Not Quite Lisp");
+	let year = puzzle.get_year();
+	let day = puzzle.get_day();
+	let name = puzzle.get_name();
+	println!("Year {year} Day {day}: {name}");
+	let file = File::open("day1.in").expect("open input failed");
+	let mut reader = BufReader::new(file);
+	let mut line = String::new();
+	let mut floor: i32 = 0;
+	reader.read_line(&mut line).expect("read line failed");
+	let chars = line.chars();
+	let mut result = Ok(());
+	for char in chars {
+		match char {
+			'(' => floor += 1,
+			')' => floor -= 1,
+			_ => { result = Err(format!("invalid character")); break }
+		}
+	}
+	println!("floor {floor}");
+	result
+}
+
+pub fn day1part2() -> Result<(), String> {
+	println!("Part Two");
+	let file = File::open("day1.in").expect("open input failed");
+	let mut reader = BufReader::new(file);
+	let mut line = String::new();
+	let mut floor: i32 = 0;
+	reader.read_line(&mut line).expect("read line failed");
+	let chars = line.chars();
+	let mut result = Ok(());
+	let mut basement = false;
+	let mut position: i32 = 0;
+	for char in chars {
+		if !basement {
+			position += 1;
+		}
+		match char {
+			'(' => floor += 1,
+			')' => floor -= 1,
+			_ => { result = Err(format!("invalid character")); break }
+		}
+		if floor == -1 {
+			basement = true;
+		}
+	}
+	println!("position {position}");
+	result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use adventofcode::Puzzle;
-
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use adventofcode::{day1part1, day1part2};
 
 fn main() {
     println!("Advent of Code");
@@ -13,56 +10,4 @@ fn main() {
 		Err(message) => println!("Error: {message}"),
 		_ => {}
 	}
-}
-
-fn day1part1() -> Result<(), String> {
-	let puzzle = Puzzle::new(2015, 1, "Not Quite Lisp");
-	let year = puzzle.get_year();
-	let day = puzzle.get_day();
-	let name = puzzle.get_name();
-	println!("Year {year} Day {day}: {name}");
-	let file = File::open("day1.in").expect("open input failed");
-	let mut reader = BufReader::new(file);
-	let mut line = String::new();
-	let mut floor: i32 = 0;
-	reader.read_line(&mut line).expect("read line failed");
-	let chars = line.chars();
-	let mut result = Ok(());
-	for char in chars {
-		match char {
-			'(' => floor += 1,
-			')' => floor -= 1,
-			_ => { result = Err(format!("invalid character")); break }
-		}
-	}
-	println!("floor {floor}");
-	result
-}
-
-fn day1part2() -> Result<(), String> {
-	println!("Part Two");
-	let file = File::open("day1.in").expect("open input failed");
-	let mut reader = BufReader::new(file);
-	let mut line = String::new();
-	let mut floor: i32 = 0;
-	reader.read_line(&mut line).expect("read line failed");
-	let chars = line.chars();
-	let mut result = Ok(());
-	let mut basement = false;
-	let mut position: i32 = 0;
-	for char in chars {
-		if !basement {
-			position += 1;
-		}
-		match char {
-			'(' => floor += 1,
-			')' => floor -= 1,
-			_ => { result = Err(format!("invalid character")); break }
-		}
-		if floor == -1 {
-			basement = true;
-		}
-	}
-	println!("position {position}");
-	result
 }
